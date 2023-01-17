ebb78ad872b45403d1d5fb68210548a21c0dee0b
WinPooh32 <winpooh32@yandex.ru>
2023-01-17 21:59:39 +1000

    Add tests for Sum

--- aggregation_test.go
+++ aggregation_test.go
@@ -130,11 +130,81 @@ func TestSum(t *testing.T) {
 		args args
 		want DType
 	}{
-		// TODO: Add test cases.
+		{
+			name: "empty",
+			args: args{
+				data: MakeData(1, []int64{}, []DType{}),
+			},
+			want: NaN,
+		},
+		{
+			name: "all NaN",
+			args: args{
+				data: MakeData(1, []int64{1, 2, 3}, []DType{NaN, NaN, NaN}),
+			},
+			want: NaN,
+		},
+		{
+			name: "one value",
+			args: args{
+				data: MakeData(1, []int64{1}, []DType{2}),
+			},
+			want: 2,
+		},
+		{
+			name: "simple",
+			args: args{
+				data: MakeData(1, []int64{1, 2, 3, 4}, []DType{2, 2, 6, 6}),
+			},
+			want: 16,
+		},
+		{
+			name: "NaNs leading",
+			args: args{
+				data: MakeData(1, []int64{1, 2, 3, 4}, []DType{NaN, NaN, 2, 6}),
+			},
+			want: 8,
+		},
+		{
+			name: "nonNaNs leading",
+			args: args{
+				data: MakeData(1, []int64{1, 2, 3, 4}, []DType{2, 6, NaN, NaN}),
+			},
+			want: 8,
+		},
+		{
+			name: "NaN at mid 1",
+			args: args{
+				data: MakeData(1, []int64{1, 2, 3, 4, 5}, []DType{2, 6, NaN, 2, 6}),
+			},
+			want: 16,
+		},
+		{
+			name: "NaN at mid 2",
+			args: args{
+				data: MakeData(1, []int64{1, 2, 3, 4, 5, 6}, []DType{2, 6, NaN, NaN, 2, 6}),
+			},
+			want: 16,
+		},
+		{
+			name: "NaN bounds",
+			args: args{
+				data: MakeData(1, []int64{1, 2, 3, 4, 5, 6}, []DType{NaN, 2, 6, 2, 6, NaN}),
+			},
+			want: 16,
+		},
+		{
+			name: "NaN bounds 2",
+			args: args{
+				data: MakeData(1, []int64{1, 2, 3, 4, 5, 6, 7, 8}, []DType{NaN, NaN, 2, 6, 2, 6, NaN, NaN}),
+			},
+			want: 16,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Sum(tt.args.data); !fpEq(got, tt.want, EpsFp32) {
+			got := Sum(tt.args.data)
+			if !(IsNA(got) && IsNA(tt.want)) && !fpEq(got, tt.want, EpsFp32) {
 				t.Errorf("Sum() = %v, want %v", got, tt.want)
 			}
 		})
